db09a03b3c6125373d198e283a454020f610ec06
Son Nguyen <hoangson091104@gmail.com>
2025-04-03 22:28:35 -0400

    WIP: Frontend Done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "media",
+  theme: {
+    extend: {
+      backgroundColor: {
+        card: "var(--card)",
+      },
+      textColor: {
+        "card-foreground": "var(--card-foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
