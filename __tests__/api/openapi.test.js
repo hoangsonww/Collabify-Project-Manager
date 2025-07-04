6bcb7bff95a4f2732096ff6b464411636c7d1faf
Son Nguyen <hoangson091104@gmail.com>
2025-07-04 14:45:03 -0400

    feat(ci/cd): add CI/CD pipeline

---
+++ __tests__/api/openapi.test.js
@@ -0,0 +1,10 @@
+const { createMocks } = require('node-mocks-http')
+const handler = require('../../pages/api/openapi').default
+jest.mock('../../utils/openapiSpec', () => ({ ok: true }))
+
+test('openapi returns spec', async () => {
+  const { req, res } = createMocks()
+  await handler(req, res)
+  expect(res._getStatusCode()).toBe(200)
+  expect(res._getJSONData()).toEqual({ ok: true })
+})
