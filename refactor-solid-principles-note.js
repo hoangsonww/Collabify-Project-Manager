1d1efeebf857370aa49aa67fb88afbb1521c2b2b
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:06 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'Collabify-Project-Manager';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
